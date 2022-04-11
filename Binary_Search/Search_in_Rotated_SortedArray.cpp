8569034fe088a9183cfc92998f6ba83e2015a460
prabalgautam01 <55079251+prabalgautam01@users.noreply.github.com>
2022-04-12 02:05:45 +0530

    Create Search_in_Rotated_SortedArray.cpp

---
+++ Binary_Search/Search_in_Rotated_SortedArray.cpp
@@ -0,0 +1,26 @@
+int search(vector<int>& nums, int target) {
+int n=arr.size();
+        int start=0,end=n-1;
+        int mid=-1;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(arr[mid]==target)
+                return mid;
+            if(arr[start]<=arr[mid] && arr[mid]>=arr[end]){
+                if(arr[start]<=target && target<=arr[mid])  
+                    end=mid-1;
+                else start=mid+1;
+            }
+            else if(arr[start]>=arr[mid] && arr[mid]<=arr[end]){
+                if(arr[mid]<=target && target<=arr[end])
+                        start=mid+1;
+                else end=mid-1;
+            }
+            else {
+                if(target<=arr[mid])
+                        end=mid-1;
+                else start=mid+1;
+            }
+        }
+        return -1;
+}
