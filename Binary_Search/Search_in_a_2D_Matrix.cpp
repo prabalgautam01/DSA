9be792fe6e7d62117298190fc66267a20d0a5aed
prabalgautam01 <55079251+prabalgautam01@users.noreply.github.com>
2022-04-15 00:49:36 +0530

    Create Search_in_a_2D_Matrix.cpp

---
+++ Binary_Search/Search_in_a_2D_Matrix.cpp
@@ -0,0 +1,21 @@
+ bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int r=0;
+        int c= matrix[0].size()-1;
+        bool ans;
+        while(r<matrix.size() && c>=0)
+        {
+            if(matrix[r][c]==target)
+            {
+                return true;
+            }
+            else if(matrix[r][c]>target)
+            {
+                c--;
+            }
+            else
+                r++;
+        }
+        return false;
+       
+    }
+     
